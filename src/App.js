b9b2fce3fbce028bf49bb31a76004bbc72f6a5c7
shams-aaliya <shamsaaliya08@gmail.com>
2021-07-17 20:20:13 +0530

    adding nav component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar.jsx'
 
 class App extends Component {
 
   render(){
   return (
       <div className="App">
+      <Navbar />
       <h1>Hello it's a new day</h1>
       </div>
     );
