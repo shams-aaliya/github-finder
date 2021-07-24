112788f9d49186df159e7bf8c62fab491f9c363b
shams-aaliya <shamsaaliya08@gmail.com>
2021-07-24 14:18:19 +0530

    Moved state fromUseritem component to parent App compoent and installed axios and used get to fetch data from the API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar.jsx';
-import Users from './components/users/Users.jsx' 
+import Users from './components/users/Users.jsx';
+import axios from 'axios'; 
 
 class App extends Component {
+state = {
+  users: [],
+  loading: false
+}
+
+async componentDidMount() {
+  this.setState({loading: true});
+  const res = await axios.get('https://api.github.com/users');
+  this.setState({users: res.data, loading: false})
+}
 
   render(){
   return (
       <div className="App">
       <Navbar />
       <div className="container">
-      <Users />
+      <Users loading={this.state.loading} users={this.state.users}/>
       </div>
       </div>
     );
