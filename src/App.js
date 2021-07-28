f95aeff3914e9f8b54e9a0c05ae72c67ec825048
shams-aaliya <shamsaaliya08@gmail.com>
2021-07-28 22:24:00 +0530

    Router added with about page!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
 import Navbar from './components/layout/Navbar.jsx';
+import About from './components/pages/About'
 import Alert from './components/layout/Alert'
 import Search from './components/users/Search';
 import Users from './components/users/Users.jsx';
@@ -33,16 +35,25 @@ setTimeout(() => {this.setState({alert:null})},5000)
     const {loading, users} = this.state
 
   return (
+    <Router>
       <div className="App">
       <Navbar />
       <div className="container">
       <Alert alert={this.state.alert}/>
-      <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers}
-      showClear={users.length>0 ? true : false}
-      setAlert={this.setAlert}/>
-      <Users loading={loading} users={users}/>
+      <Switch>
+      <Route exact path='/' render={props => (
+        <Fragment>
+        <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers}
+        showClear={users.length>0 ? true : false}
+        setAlert={this.setAlert}/>
+        <Users loading={loading} users={users}/>
+        </Fragment>
+        )}/>
+        <Route exact path='/about' component={About}/>
+      </Switch>
       </div>
       </div>
+      </Router>
     );
   }
 }
